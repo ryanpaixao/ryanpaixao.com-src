10c81710058a1f5e11fd843aa29911308a3044be
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2025-10-07 19:41:36 -0300

    add: skeleton-toggle to theme-switcher

---
+++ app/components/ui/skeletons/skeleton-toggle.tsx
@@ -0,0 +1,9 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export function SkeletonToggle() {
+  return (
+    <div>
+      <Skeleton className="h-[36px] w-[36px]" />
+    </div>
+  );
+};
--- app/components/ui/theme-switcher.tsx
+++ app/components/ui/theme-switcher.tsx
@@ -4,7 +4,10 @@ import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 import { Moon as MoonIcon, Sun as SunIcon } from 'lucide-react';
 
+// Shadcn/ui 
 import { Button } from "@/components/ui/button";
+
+import { SkeletonToggle } from "./skeletons/skeleton-toggle";
 
 export default function ThemeSwitcher() {
   const [mounted, setMounted] = useState(false);
@@ -13,7 +16,7 @@ export default function ThemeSwitcher() {
   // Ensure component is mounted to avoid hydration mismatch
   useEffect(() => setMounted(true), []);
 
-  if (!mounted) return null;
+  if (!mounted) return <SkeletonToggle />;
 
   return (
     <Button size='icon' variant='ghost' onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
