f1bdb8ba68c57a31feff82501227517d5bee5082
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2025-10-15 23:48:49 -0300

    add: interactive-code-editor to Home Page

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,5 +1,4 @@
-// This is the About page. Path = '/'
-
+import InteractiveCodeEditor from "@/app/components/ui/interactive-code-editor/interactive-code-editor";
 import { getDictionary } from "@/app/lib/dictionaries";
 import { LangParams } from "@/app/lib/definitions";
 
@@ -12,8 +11,30 @@ export default async function Home({ params }: PageParams) {
   const dict = await getDictionary(lang);
 
   return (
-    <div>
-      Home Page ({dict.siteHeader.nav.links.about}?)
+    <div className="home-container">
+      <div className="hero-section">
+        <div className="hero-content">
+          <h1>
+            {dict.homePage.hello}
+          </h1>
+          <p className="hero-subtitle">
+            {dict.homePage.description}
+          </p>
+
+          {/** Interactive Code Editor */}
+          <InteractiveCodeEditor dict={dict} />
+
+          {/** Call to Action Buttons */}
+          {/* <div className="cta-buttons">
+            <button className="btn btn-primary">
+              {dict.homePage.primaryCTA}
+            </button>
+            <button className="btn btn-primary">
+              {dict.homePage.secondaryCTA}
+            </button>
+          </div> */}
+        </div>
+      </div>
     </div>
   );
 }
