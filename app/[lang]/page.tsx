19ad3b70469869ec766511bbd130fbe2b56f16f1
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2025-11-05 21:19:39 -0300

    add: CTA to main page

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import InteractiveCodeEditor from "@/app/components/ui/interactive-code-editor/interactive-code-editor";
 import { getDictionary } from "@/app/lib/dictionaries";
 import { LangParams } from "@/app/lib/definitions";
@@ -22,17 +24,27 @@ export default async function Home({ params }: PageParams) {
           </p>
 
           {/** Interactive Code Editor */}
-          <InteractiveCodeEditor dict={dict} />
+          <div className="mt-5 mb-5">
+            <InteractiveCodeEditor dict={dict} />
+          </div>
 
           {/** Call to Action Buttons */}
-          {/* <div className="cta-buttons">
-            <button className="btn btn-primary">
-              {dict.homePage.primaryCTA}
-            </button>
-            <button className="btn btn-primary">
-              {dict.homePage.secondaryCTA}
-            </button>
-          </div> */}
+          <div className="flex flex-auto space-between cta-buttons w-full">
+            <div className="mr-3">
+              <button className="btn btn-primary">
+                <Link href={`/${lang}/projects`}>
+                  {dict.homePage.primaryCTA}
+                </Link>
+              </button>
+            </div>
+            <div>
+              <button className="btn btn-primary">
+                <Link href={`/${lang}/contact`}>
+                  {dict.homePage.secondaryCTA}
+                </Link>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
