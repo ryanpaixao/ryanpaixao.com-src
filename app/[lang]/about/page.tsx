fd731f88029697b9ce2c12f14ec670b0195131f2
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2025-11-24 20:15:40 -0300

    add: ryan img to about page. Update about styling

--- app/[lang]/about/page.tsx
+++ app/[lang]/about/page.tsx
@@ -1,6 +1,10 @@
+import Image from 'next/image';
+
 import PageHeaderTitle from '@/app/components/ui/page-header-title';
 import { LangParams } from "@/app/lib/definitions";
 import { getDictionary } from "@/app/lib/dictionaries";
+import PageContentWrapper from "@/app/components/ui/page-content-wrapper";
+
 
 interface AboutPageProps {
   params: LangParams;
@@ -11,22 +15,37 @@ export default async function Page({ params }: AboutPageProps) {
   const dict = await getDictionary(lang);
 
   return (
-    <div>
-      <PageHeaderTitle>
-        {dict.aboutPage.pageHeaderTitle}
-      </PageHeaderTitle>
-      <div>
-        {dict.aboutPage.aboutParagraphs.map((paragraph, index) => {
-          return (
-            <p
-              key={`about-para-${index}`}
-              className='mb-3'
-            >
-              {paragraph}
-            </p>
-          )
-        })}
-      </div>
-    </div>
+    <PageContentWrapper>
+      <div className='flex flex-col justify-center'>
+        <div className='text-center'>
+          <PageHeaderTitle>
+            {dict.aboutPage.pageHeaderTitle}
+          </PageHeaderTitle>
+        </div>
+        <div>
+            <div className='flex justify-center justify-items-center mb-6'>
+              <Image
+                src="/images/ryan_1.webp"
+                alt="Image of Ryan"
+                width={338}
+                height={313}
+                priority
+              />
+            </div>
+            <div className='mt-6'>
+              {dict.aboutPage.aboutParagraphs.map((paragraph, index) => {
+                return (
+                  <p
+                    key={`about-para-${index}`}
+                    className='mb-3'
+                  >
+                    {paragraph}
+                  </p>
+                )
+              })}
+            </div>
+          </div>
+        </div>
+    </PageContentWrapper>
   );
 }
