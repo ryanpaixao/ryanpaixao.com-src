27f1b983079b4b6cf1063bcfe12ae794b29a6063
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2025-11-24 19:45:12 -0300

    update: home page styling

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -13,23 +13,25 @@ export default async function Home({ params }: PageParams) {
   const dict = await getDictionary(lang);
 
   return (
-    <div className="home-container">
-      <div className="hero-section">
+    <div className="home-container font-sans grid grid-rows-[56px_1fr_20px] items-center justify-items-center min-h-screen gap-4">
+      <div className="hero-section flex flex-col row-start-2 items-center sm:items-start">
         <div className="hero-content">
-          <h1>
-            {dict.homePage.hello}
-          </h1>
-          <p className="hero-subtitle">
-            {dict.homePage.description}
-          </p>
+          <div>
+            <h1 className='text-center mx-2'>
+              {dict.homePage.hello}
+            </h1>
+            <p className="hero-subtitle text-center mx-2">
+              {dict.homePage.description}
+            </p>
 
-          {/** Interactive Code Editor */}
-          <div className="mt-5 mb-5">
-            <InteractiveCodeEditor dict={dict} />
+            {/** Interactive Code Editor */}
+            <div className="mt-5 mb-5">
+              <InteractiveCodeEditor dict={dict} />
+            </div>
           </div>
 
           {/** Call to Action Buttons */}
-          <div className="flex flex-auto space-between cta-buttons w-full">
+          <div className="flex flex-auto space-between cta-buttons w-full justify-center">
             <div className="mr-3">
               <button className="btn btn-primary">
                 <Link href={`/${lang}/projects`}>
